2dd506ca860651cd5d3cb9c81e76691abd4eaa4a
srivenkatesh92 <srivenkatesh123@gmail.com>
2021-06-27 12:48:01 +0530

    service arch added in application
    new service added for common functionality

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonUtilsService } from '../common-utils.service';
 
 @Component({
   selector: 'app-header',
@@ -7,17 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private CommonUtilsService: CommonUtilsService) { }
 
   imagePath = './assets/img/background/bg-code-';
-  backgroundImage = '';
-
-  randomNumber = function(min : any, max : any) {
-    return Math.floor(Math.random() * (max - min) + min);
-  }
+  backgroundImage = ''; 
 
   ngOnInit(): void {
-    this.backgroundImage = this.imagePath + this.randomNumber(1,6) + '.PNG';
+    this.backgroundImage = this.imagePath + this.CommonUtilsService.getRandomNumber(1,7) + '.PNG';
     //this.backgroundImage = this.imagePath + "1" + '.gif';
   }
 
