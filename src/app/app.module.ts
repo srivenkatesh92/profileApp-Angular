5b5f1c131f2dfa2109e82325da96e7d14bc2a113
srivenkatesh92 <srivenkatesh123@gmail.com>
2025-07-29 21:15:27 +0530

    Update Angular dependencies to version 18.2.13 and refactor HttpClientModule usage in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,28 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CertificationComponent } from './certification/certification.component';
 
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { HeaderComponent } from './header/header.component';
 import { CommonUtilsService } from './common-utils.service';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    CertificationComponent,
-    FooterComponent,
-    AboutComponent,
-    ExperienceComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [CommonUtilsService],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        CertificationComponent,
+        FooterComponent,
+        AboutComponent,
+        ExperienceComponent,
+        HeaderComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule], providers: [CommonUtilsService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
